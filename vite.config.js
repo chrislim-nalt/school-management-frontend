336a5e330eda27452eedba8d7d1d57aba3258a37
Chrislim Nalt <kenlychris72@gmail.com>
2026-06-23 18:22:22 +0200

    Update the designs for school features

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,64 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
+  
   return {
     plugins: [react()],
+    
     server: {
       port: 5173,
       open: true
     },
+    
     build: {
       outDir: 'dist',
-      sourcemap: true
+      sourcemap: false,
+      chunkSizeWarningLimit: 2000,
+      minify: 'terser',
+      terserOptions: {
+        compress: {
+          drop_console: true,
+          drop_debugger: true,
+        }
+      },
+      rollupOptions: {
+        output: {
+          chunkFileNames: 'assets/js/[name]-[hash].js',
+          entryFileNames: 'assets/js/[name]-[hash].js',
+          assetFileNames: 'assets/css/[name]-[hash].[ext]',
+          manualChunks: {
+            'react-vendor': [
+              'react', 
+              'react-dom', 
+              'react-router-dom'
+            ],
+            'file-handlers': [
+              'file-saver', 
+              'xlsx', 
+              'jspdf', 
+              'jspdf-autotable'
+            ],
+            'http-client': ['axios'],
+          }
+        }
+      }
     },
+    
     define: {
       'import.meta.env.VITE_API_URL': JSON.stringify(env.VITE_API_URL)
+    },
+    
+    optimizeDeps: {
+      include: [
+        'react',
+        'react-dom',
+        'react-router-dom',
+        'axios',
+        'file-saver',
+        'xlsx',
+        'jspdf',
+        'jspdf-autotable'
+      ]
     }
   }
 })
